5c969810ce706e254f43cab47297d53ed0540885
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-02 00:56:18 +0600

    Add files via upload

--- src/routes/adminCryptoRoutes.js
+++ src/routes/adminCryptoRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { adminListCryptoMethods, adminUpdateCryptoMethod } from '../controllers/cryptoController.js';
+import { adminListCryptoMethods, adminSyncCryptoSubscriptions, adminUpdateCryptoMethod } from '../controllers/cryptoController.js';
 import { protect, requireAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/crypto-methods', protect, requireAdmin, adminListCryptoMethods);
 router.patch('/crypto-methods/:key', protect, requireAdmin, adminUpdateCryptoMethod);
+router.post('/crypto/subscriptions/sync', protect, requireAdmin, adminSyncCryptoSubscriptions);
 
 export default router;
