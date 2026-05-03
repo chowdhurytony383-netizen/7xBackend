58ac445d9c932cc8177d271fe8867dfda7407ae3
chowdhurytony383-netizen <chowdhurytony383@gmail.com>
2026-05-03 17:56:23 +0600

    Add files via upload

--- src/routes/vgamesRoutes.js
+++ src/routes/vgamesRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { asyncHandler } from '../utils/asyncHandler.js';
-import { handleVGameAction } from '../controllers/vgamesController.js';
+import { handleVGameAction, handleVGameHistoryDetail } from '../controllers/vgamesController.js';
 
 const router = Router();
 
+router.get('/history/:betId', asyncHandler(handleVGameHistoryDetail));
 router.all('/:token/:action', asyncHandler(handleVGameAction));
 
 export default router;
